c01724604cf5b86213f366e9bb2c731713f44860
Fernando Luiz Goulart <fernando@leapi.com>
2021-08-01 16:09:45 -0300

    Deletion returns nil completion

--- FeedStoreChallenge/CoreDataFeedStore.swift
+++ FeedStoreChallenge/CoreDataFeedStore.swift
@@ -59,7 +59,7 @@ public final class CoreDataFeedStore: FeedStore {
 	}
 
 	public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-		fatalError("Must be implemented")
+		completion(nil)
 	}
 
 	// MARK: Helpers
