9f1765543416e5942143acd16af74d6074b74183
Fernando Luiz Goulart <fernando@leapi.com>
2021-08-02 15:19:22 -0300

    Removes explicit internal access-control definition

--- FeedStoreChallenge/Infrastructure/ManagedFeedImage.swift
+++ FeedStoreChallenge/Infrastructure/ManagedFeedImage.swift
@@ -18,7 +18,7 @@ class ManagedFeedImage: NSManagedObject {
 }
 
 extension ManagedFeedImage {
-	internal static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
+	static func images(from localFeed: [LocalFeedImage], in context: NSManagedObjectContext) -> NSOrderedSet {
 		return NSOrderedSet(array: localFeed.map { local in
 			let managed = ManagedFeedImage(context: context)
 			managed.id = local.id
@@ -29,7 +29,7 @@ extension ManagedFeedImage {
 		})
 	}
 
-	internal var local: LocalFeedImage {
+	var local: LocalFeedImage {
 		return LocalFeedImage(id: id, description: imageDescription, location: location, url: url)
 	}
 }
